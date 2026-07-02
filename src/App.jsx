a41eca13895d3aecd905c621ba8411308d378b55
joshuakaufman3827 <148819008+joshuakaufman3827@users.noreply.github.com>
2026-07-02 14:24:22 -0500

    updated react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1>My Todo List</h1>
+    <div>
+      <h2>Finish Frontend Simplified</h2>
+      <button>Delete</button>
+    </div>
+   </div> 
+  );    
+}
+
+export default App;
