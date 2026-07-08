81508249766be5cbc683756e3cc664dc9519e91f
joshuakaufman3827 <148819008+joshuakaufman3827@users.noreply.github.com>
2026-07-08 15:50:46 -0500

    final react rocketship update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function Home () {
-    return <h1>Home</h1>
+    const [users, setUsers] = useState([])
+
+    async function fetchUsers() {
+     const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+     setUsers(data);
+   }
+
+   useEffect(() => {
+        setTimeout(() => {
+            fetchUsers();
+        }, 500);
+    }, [])
+
+    const pixels = "3px";
+
+    return (
+      <div>
+        {users.map((user) => {
+         return (
+        <div key={user.id} style={{ border: `${pixels} solid black` }}>
+         <div>{user.id}</div>
+         <div>{user?.name}</div>
+         <div>{user?.email}</div>
+         <div>{user?.username}</div>
+       </div>
+     );
+    })}
+   </div>
+ );
 }
 
 export default Home
